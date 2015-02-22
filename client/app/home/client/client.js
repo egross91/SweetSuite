f844cc536e29a386e8c9d5a0cf65fcd2b6404850
Juan Nunez <njuan1993@yahoo.com>
2015-02-21 22:52:04 -0500

    Added two buttons

--- client/app/home/client/client.js
+++ client/app/home/client/client.js
@@ -1,3 +1,17 @@
 /**
  * Created by juan on 2/21/15.
  */
+var count=0;
+
+function show_image(src, width, height, alt) {
+  if(count<1) {
+    var img = document.createElement("img");
+    img.src = src;
+    img.width = width;
+    img.height = height;
+    img.alt = alt;
+
+    document.body.appendChild(img);
+  }
+  count++;  //makes sure that the element only get appended once
+}
