126cc15cd0da8e77c3ca972e51718a0eb781c803
Sahir Boghani <sahirboghani@gmail.com>
2015-02-21 20:24:19 -0500

    linked home page after login

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -4,7 +4,7 @@ angular.module('sweetSuiteApp')
   .controller('NavbarCtrl', function ($scope, $location, Auth) {
     $scope.menu = [{
       'title': 'Home',
-      'link': '/'
+      'link': '/home'
     },
       {
         'title': 'How It Works',
