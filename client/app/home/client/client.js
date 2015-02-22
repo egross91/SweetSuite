a55c521ed94e7a238862c96a7187a1bf5fddc58f
Sahir Boghani <sahirboghani@gmail.com>
2015-02-22 01:38:11 -0500

    changes on client page

--- client/app/home/client/client.js
+++ client/app/home/client/client.js
@@ -1,10 +1,13 @@
 /**
  * Created by juan on 2/21/15.
  */
-var count=0;
+
+var exampleExists = false;
+var name;
+var tasks = [];
 
 function show_image(src, width, height, alt) {
-  if(count<1) {
+  if(!exampleExists) {
     var img = document.createElement("img");
     img.src = src;
     img.width = width;
@@ -13,5 +16,41 @@ function show_image(src, width, height, alt) {
 
     document.body.appendChild(img);
   }
-  count++;  //makes sure that the element only get appended once
+  exampleExists = true;  //makes sure that the element only get appended once
+}
+
+// ask for name of list and then tasks
+function list_info() {
+  remove_lists();
+  name = prompt("Please enter list name", "List Name");
+  var temp = "a";
+  while(temp != "done") {
+    temp = prompt("Enter task, enter \"done\" when done", "done");
+    tasks.push(temp);
+  }
+}
+
+// post name and tasks
+/*
+
+TODO
+post text on same page, not new one
+post with new lines in between tasks
+
+ */
+function post_list() {
+  document.write(name + "\n");
+  for(var i = 0; i < tasks.length-1; i++) {
+    document.write(tasks[i] + "\n");
+  }
+}
+
+
+function remove_lists() {
+  var images = document.getElementsByTagName('img');
+  var l = images.length;
+  for (var i = 0; i < l; i++) {
+    images[0].parentNode.removeChild(images[0]);
+  }
+  exampleExists = false;
 }
