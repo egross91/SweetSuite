ce8379b4c59337a3fbb8879d8f5ec03a64274407
Eric <eric.bgross@gmail.com>
2015-02-24 17:43:50 -0500

    [Protractor]Added smoke tests and client smoke test

--- e2e/main/main.po.js
+++ e2e/main/main.po.js
@@ -9,6 +9,19 @@ var MainPage = function() {
   this.heroEl = element(by.css('.hero-unit'));
   this.h1El = this.heroEl.element(by.css('h1'));
   this.imgEl = this.heroEl.element(by.css('img'));
+
+  this.navbarEl = element(by.id('navbar-main'));
+  this.loginEl = this.navbarEl.element(by.id('login'));
+
+  this.loginFormEl = element(by.name('form'));
+  this.emailEl = this.loginFormEl.element(by.name('email'));
+  this.passwordEl = this.loginFormEl.element(by.name('password'));
+  this.loginBtnEl = this.loginFormEl.element(by.id('loginBtn'));
+
+  this.clientBtnListEl = element(by.id('btnListsDiv'));
+  this.createTodoListBtn = this.clientBtnListEl.element(by.id('createListBtn'));
+  this.viewExampleListBtn = this.clientBtnListEl.element(by.id('viewExampleListBtn'));
+  this.removeListsBtn = this.clientBtnListEl.element(by.id('removeListsBtn'));
 };
 
 module.exports = new MainPage();
--- e2e/main/main.spec.js
+++ e2e/main/main.spec.js
@@ -13,4 +13,16 @@ describe('Main View', function() {
     expect(page.imgEl.getAttribute('src')).toMatch(/assets\/images\/MadeSuite.png$/);
     expect(page.imgEl.getAttribute('alt')).toBe('( ͡° ͜ʖ ͡°)');
   });
+
+  it('should login the user in and take them to the client page with the base elmeents showing', function() {
+    //page.navbarEl.getText();
+    page.loginEl.click();
+    page.emailEl.sendKeys('test@test.com');
+    page.passwordEl.sendKeys('test');
+    page.loginBtnEl.click();
+
+    expect(page.createTodoListBtn.getText()).toBe('Create A TO-DO List');
+    expect(page.viewExampleListBtn.getText()).toBe('View Example List');
+    expect(page.removeListsBtn.getText()).toBe('Remove Lists');
+  });
 });
