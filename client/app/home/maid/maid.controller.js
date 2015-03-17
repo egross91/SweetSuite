e05484c44365ef55cb4f7bedd31c8f35efe6c096
Juan Nunez <njuan1993@yahoo.com>
2015-03-16 20:55:10 -0400

    Small maid skeleton. Added my interpretation of what else needs to be done

--- client/app/home/maid/maid.controller.js
+++ client/app/home/maid/maid.controller.js
@@ -2,6 +2,19 @@
 
 angular.module('sweetSuiteApp')
   .controller('MaidCtrl', function ($scope, socket, $http, Modal, _, User, $location, $anchorScroll, Auth) {
+    $scope.client = User.get();
+
+    $scope.toggleUserList = function(){
+      //should list all users linked to the maid.
+      //I believe admins must assign the user to maid.
+    }
+    //**Needs to be implemented**//
+
+    //After the maid can toggle its userList, it should be able to click on a user
+    //and view that users active list(The user may have various list but may only wish to
+    //display some to the maid(the ones that he/she needs to accomplish
+    //The maid may then view the lists and check off the tasks as done.
+
 
 
   });
