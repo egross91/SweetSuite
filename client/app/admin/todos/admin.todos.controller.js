446262d2ba69e18d03c92871ab9363bd0a72f2f0
Eric <eric.bgross@gmail.com>
2015-03-17 23:54:07 -0400

    [Admin-Todos] Fixed bug with showing !users to show in Admin list of users

--- client/app/admin/todos/admin.todos.controller.js
+++ client/app/admin/todos/admin.todos.controller.js
@@ -2,16 +2,10 @@
 
 angular.module('sweetSuiteApp')
   .controller('AdminTodosCtrl', function ($scope, $http, Auth, User, _, $location, $anchorScroll) {
-    $scope.clients = User.query(function(users) {
-      angular.forEach(users, function(u, i) {
-        if (u.role !== 'user') {
-          users.splice(i, 1);
-        }
-      });
-    });
-
-    $scope.focusUser = function(name) {
-      var aref = 'user' + name;
+    /**
+     * PRIVATE API
+     */
+    var anchor = function(aref) {
       if ($location.hash() !== aref) {
         $location.hash(aref);
       } else {
@@ -19,4 +13,41 @@ angular.module('sweetSuiteApp')
       }
     };
 
+    var filterByRole = function(users, role) {
+      var roleUsers = [];
+
+      angular.forEach(users, function(u) {
+        if (u.role === role) {
+          roleUsers.push(u);
+        }
+      });
+
+      $scope.clients = roleUsers;
+    };
+
+    /**
+     * CONTROLLER PROPERTIES
+     */
+    $scope.clients = User.query().$promise.then(function(users) {
+      filterByRole(users, 'user');
+    });
+
+    /**
+     * PUBLIC API
+     */
+    $scope.deleteList = function(client, list) {
+      angular.forEach(list.todos, function(l, i) {
+        // TODO
+      });
+    };
+
+    $scope.focusUser = function(name) {
+      var aref = 'user' + name;
+      anchor(aref);
+    };
+
+    $scope.focusList = function(user, name) {
+      var aref = 'todoList' + user + name;
+      anchor(aref);
+    };
   });
