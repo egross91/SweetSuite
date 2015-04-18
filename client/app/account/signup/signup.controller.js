e223f35f20e5176646ab1c5934755321559632c3
Chris <cmbusse@comcast.net>
2015-04-18 16:51:33 -0400

    Modified sign up to allow signing up as maid or client

--- client/app/account/signup/signup.controller.js
+++ client/app/account/signup/signup.controller.js
@@ -12,7 +12,8 @@ angular.module('sweetSuiteApp')
         Auth.createUser({
           name: $scope.user.name,
           email: $scope.user.email,
-          password: $scope.user.password
+          password: $scope.user.password,
+          role: $scope.user.role
         })
         .then( function() {
           // Account created, redirect to home
