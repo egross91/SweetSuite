6bb2b804fd41ac220c10f56737014f17d90b613b
Chris <cmbusse@comcast.net>
2015-04-21 22:17:19 -0400

    Some work on Assignming Maids, not yet functioning

--- client/app/admin/maids/admin.maids.controller.js
+++ client/app/admin/maids/admin.maids.controller.js
@@ -6,4 +6,21 @@ angular.module('sweetSuiteApp')
   	$scope.maids = User.query();
     $scope.errors = {};
 
+    $scope.addClient = function(maid, newClient){
+    	/*
+		*
+    	Idea was to use localStorage variables to keep track of the email
+    	addresses of clients associated to a maid, but currently unable to
+    	get it working
+		*
+    	*/
+
+    	/*
+    	var maidEmail = maid.email;
+    	var temp = localStorage[maidEmail];
+    	localStorage[maidEmail] = temp + | + newClient;
+		*/
+
+    };
+
   });
